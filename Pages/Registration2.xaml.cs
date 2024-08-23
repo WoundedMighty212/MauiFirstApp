966c828ddeaf0e62b3b10d61a37a3bf3df1c03de
Michael Ludick <michael.ludick13@outlook.com>
2024-08-23 14:34:36 +0200

    search bars and dropdowns and radion buttons added to reg page 2

--- Pages/Registration2.xaml.cs
+++ Pages/Registration2.xaml.cs
@@ -6,4 +6,39 @@ public partial class Registration2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Race_Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void FemaleRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void MaleRadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void Country_Editor_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Province_Editor_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void City_Editor_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Suburb_Editor_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
 }
