7adc75acedbde540feb70081c33fbefca0aab045
Michael Ludick <michael.ludick13@outlook.com>
2024-08-24 00:11:46 +0200

    login view added

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -1,9 +1,13 @@
 namespace TestApp.Pages;
-
 public partial class Login : ContentPage
 {
 	public Login()
 	{
 		InitializeComponent();
 	}
+
+    private void LoginBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
